1217a1bafa5b1705e8936da8825cfd825d2f1416
wgspacecan <>
2021-08-27 18:30:44 -0700

    readme and checkers

---
+++ checkers/checkers.hpp
@@ -0,0 +1,78 @@
+/*
+*   Checkers
+*    a two player game
+*
+*   change log:
+*   - 
+*   -
+*
+*/
+
+#ifndef _CHECKERS_HPP_
+#define _CHECKERS_HPP_
+
+#define DEBUG
+
+//#include <stdlib.h>
+//#include <time.h>
+#include <iostream>
+#include <vector>
+
+namespace Checkers {
+
+    void clear_screen() {
+        std::cout << std::string(100, '\n');
+    }
+
+    struct piece {
+        bool type;
+    };
+
+    class Board {
+    public:
+        void display_board() {
+
+        }
+    
+    };
+
+    class Player {
+    private:
+    public:
+        Player() {
+        }
+    };
+
+    class Game {
+    private:
+        Board board;
+        Player player1;
+        Player player2;
+        void reset() {
+            board = Board();
+            player1 = Player();
+            player2 = Player();
+        }
+        void player_life() {
+            // view board
+            // action
+        }
+    public:
+        Game() {
+        }
+        void run() {
+            reset();
+            bool game_on = true;
+            while(game_on) {
+                // player 1
+                // player 2
+
+                // detect legal moves
+                // detect win/loss
+                // input can end game
+            }
+        }
+    };
+};
+
+#endif
---
+++ checkers/main.cpp
@@ -0,0 +1,7 @@
+#include "checkers.hpp"
+
+int main () {
+    Checkers::Game game;
+    game.run();
+    return 0;
+}
